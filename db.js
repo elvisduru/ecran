5c2a45b6a8a13d386f6c4f1ab780c1026f999bac
Elvis Duru <duruelvis95@gmail.com>
2020-06-25 12:54:15 +0100

    added atlas url

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-const uri = 'mongodb://localhost:27017/ecran'
+const uri =
+  // 'mongodb://localhost:27017/ecran'
+  'mongodb+srv://elvis:victory1@ecran-zj3ac.mongodb.net/ecran?retryWrites=true&w=majority'
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(
   () => console.log("Connected to Mongo"),
